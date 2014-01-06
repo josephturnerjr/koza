2afff2622fa52fdb04195446307e52cc4d0ceb8f
Joseph Turner <turner@miserware.com>
2014-01-06 10:28:36 -0500

    Trying to get the crossover working.

--- src/koza/core.clj
+++ src/koza/core.clj
@@ -1,8 +1,10 @@
 (ns koza.core
   (:gen-class)
-  (:use clojure.string))
+  (:require clojure.zip)
+  (:use [clojure.string]))
 
 (defn generate-tree
+    "Generates a genetic-programming individual using the 'grow' method"
     [terms funcs depth max-depth]
     (def select-from
         (if (== depth 0)
@@ -22,6 +24,48 @@
         ))))
 
 
+(defn recursive-count
+    [coll]
+    (apply + (for [el coll]
+         (if (not (seq? el))
+             1
+             (recursive-count el)
+    ))))
+
+(defn walk-steps
+    [dz steps]
+    (clojure.zip/next dz)
+    (if (= steps 0)
+        dz
+        (recur dz (- steps 1))
+    )
+)
+
+(defn replace-location
+    [m w]
+    (clojure.zip/root
+        (clojure.zip/replace
+            m
+            (clojure.zip/node w))))
+
+(defn get-random-subtree
+    [z]
+    (def walk-len (rand-int (recursive-count z)))
+    (nth 
+        (iterate clojure.zip/next z) 
+        man-walk)
+
+(defn crossover
+    [man woman]
+    (def m-zip (clojure.zip/seq-zip man))
+    (def w-zip (clojure.zip/seq-zip woman))
+    (let [mx 
+          wx (nth 
+                    (iterate clojure.zip/next w-zip) 
+                    woman-walk)]
+        [(replace-location mx wx) (replace-location wx mx)])
+)
+
 (defn generate-individual 
     [terms funcs max-depth]
     (generate-tree terms funcs 0 max-depth))
@@ -34,5 +78,5 @@
   (def funcs '((+, 2) (-, 2) (*, 2)))
   (def ret (generate-individual tterms funcs 4))
   (println ret)
-  (println (eval ret))
+  (println (first (crossover ret ret)))
 )
